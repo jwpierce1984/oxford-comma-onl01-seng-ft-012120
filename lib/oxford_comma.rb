35e998694ea0954aee8b7accaadf2dfd908a3788
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 17:24:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length < 2
+    return array
+  elsif array.length == 2
     return array.join(" and ")
   else array.lenth >= 3
     array[-1].insert(0, "and ")
