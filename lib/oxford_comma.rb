7a3e01568048e16123531259e4baa36653efda92
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 17:20:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join("and")
   else array.lenth >= 3
-  array[-1].insert(0, "and")
-  array.join(,)
+    array[-1].insert(0, "and")
+    array.join(,)
+  end
 end
